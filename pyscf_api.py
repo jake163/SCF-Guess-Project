c5362287eda0cacb63c90354b31ee7b52f06b491
Jonathan Waldrop <relik107@gmail.com>
2020-05-28 19:12:46 -0500

    basic functional should work

--- pyscf_api.py
+++ pyscf_api.py
@@ -1,16 +1,34 @@
-from pyscf import gto
-from pyscf import scf
+import pyscf
 
-def run_scf(atoms, coords, basis, density = None):
-    assert(len(atoms) == len(coords))
-
-    atomic_coords = [[atom, coord] for atom, coord in zip(atoms, coords)]
-    mol = gto.Mole()
-    mol.atom = atomic_coords
-    mol.basis = basis
-    mol.build()
-
-    mf = scf.HF(mol)
+def density(molecule, basis, verbose = 0):
+    mol = pyscf.M(atom = molecule, basis = basis)
+    mf = pyscf.scf.HF(mol)
+    mf.verbose = verbose
     mf.kernel()
-    
-    pass
+    return mf.make_rdm1()
+
+def run_scf(molecule, basis, density = None, verbose = 4):   
+    n_cycles = 0
+    def get_cycles(locl):
+        nonlocal n_cycles
+        if "cycle" in locl: n_cycles = locl["cycle"] + 1
+
+    mol = pyscf.M(atom = molecule, basis = basis)
+    mf = pyscf.scf.HF(mol)
+    mf.verbose = verbose
+    mf.callback = get_cycles
+    mf.kernel(density)
+    print(n_cycles)
+
+
+if __name__ == "__main__":
+    mol = [
+        ("H", 0.0, 0.0, 0.89),
+        ("H", 0.0, 0.0, -0.89),
+	    ("O", 0.0, 0.0, 0.0),
+	    ("H", 1.0, 0.0, 0.89),
+        ("H", 1.0, 0.0, -0.89),
+	    ("O", 1.0, 0.0, 0.0)
+    ]
+
+    run_scf(mol, "sto-3g")
