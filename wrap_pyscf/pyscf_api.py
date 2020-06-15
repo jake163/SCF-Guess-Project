7df3791dc9807a239ec64b6c8b3742f9a2f54de8
Ryan Richard <ryanmrichard1@gmail.com>
2020-06-15 14:23:01 -0500

    default to doublet for odd numbers of electrons

--- wrap_pyscf/pyscf_api.py
+++ wrap_pyscf/pyscf_api.py
@@ -1,7 +1,25 @@
 import pyscf
 
+atom2z = {'h' : 1, 'he' : 2,
+          'li' : 3, ' be' : 4, 'n' : 5, 'c': 6, 'n':7, ' o' : 8, 'f': 9, 'ne':10,
+          }
+
+def make_molecule(atoms, basis):
+    nelec = 0
+    for a in atoms:
+        at_sym = a[0].lower()
+        if at_sym not in atom2z:
+            raise "Atomic symbol {} is not in atom2z. Please add it".format(at_sym)
+        nelec += atom2z[at_sym]
+    mol = pyscf.gto.Mole()
+    mol.atom = atoms
+    mol.basis = basis
+    mol.spin = 0 if nelec % 2 == 0 else 1
+    mol.build()
+    return mol
+
 def density(molecule, basis, verbose = 0):
-    mol = pyscf.M(atom = molecule, basis = basis)
+    mol = make_molecule(molecule, basis)
     mf = pyscf.scf.HF(mol)
     mf.verbose = verbose
     mf.kernel()
@@ -13,7 +31,7 @@ def run_scf(molecule, basis, density = None, verbose = 4):
         nonlocal n_cycles
         if "cycle" in locl: n_cycles = locl["cycle"] + 1
 
-    mol = pyscf.M(atom = molecule, basis = basis)
+    mol = make_molecule(molecule, basis)
     mf = pyscf.scf.HF(mol)
     mf.verbose = verbose
     mf.callback = get_cycles
