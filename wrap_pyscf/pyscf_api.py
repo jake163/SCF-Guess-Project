4c4ca9ccd5da5ea16b34147ed8350b3e17f57eea
Ryan Richard <ryanmrichard1@gmail.com>
2020-06-23 13:58:06 -0500

    requires the user to provide the number of unpaired electrons

--- wrap_pyscf/pyscf_api.py
+++ wrap_pyscf/pyscf_api.py
@@ -1,37 +1,27 @@
 import pyscf
 
-atom2z = {'h' : 1, 'he' : 2,
-          'li' : 3, ' be' : 4, 'n' : 5, 'c': 6, 'n':7, ' o' : 8, 'f': 9, 'ne':10,
-          }
-
-def make_molecule(atoms, basis):
-    nelec = 0
-    for a in atoms:
-        at_sym = a[0].lower()
-        if at_sym not in atom2z:
-            raise "Atomic symbol {} is not in atom2z. Please add it".format(at_sym)
-        nelec += atom2z[at_sym]
+def make_molecule(atoms, basis, unpaired_electrons = 0):
     mol = pyscf.gto.Mole()
     mol.atom = atoms
     mol.basis = basis
-    mol.spin = 0 if nelec % 2 == 0 else 1
+    mol.spin = unpaired_electrons
     mol.build()
     return mol
 
-def density(molecule, basis, verbose = 0):
-    mol = make_molecule(molecule, basis)
+def density(molecule, basis, verbose = 0, unpaired_electrons = 0):
+    mol = make_molecule(molecule, basis, unpaired_electrons)
     mf = pyscf.scf.HF(mol)
     mf.verbose = verbose
     mf.kernel()
     return mf.make_rdm1()
 
-def run_scf(molecule, basis, density = None, verbose = 4):   
+def run_scf(molecule, basis, density = None, unpaired_electrons = 0, verbose = 4):
     n_cycles = 0
     def get_cycles(locl):
         nonlocal n_cycles
         if "cycle" in locl: n_cycles = locl["cycle"] + 1
 
-    mol = make_molecule(molecule, basis)
+    mol = make_molecule(molecule, basis, unpaired_electrons)
     mf = pyscf.scf.HF(mol)
     mf.verbose = verbose
     mf.callback = get_cycles
